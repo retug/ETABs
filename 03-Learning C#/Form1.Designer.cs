a68541b76fd4be39945cc4d0b9ac61e800458104
Austin Guter <45467091+retug@users.noreply.github.com>
2023-02-04 13:58:48 -0700

    Add files via upload

---
+++ 03-Learning C#/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ETABS_Plugin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ShowReactionForcesBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.LoadCombinationComBox = new System.Windows.Forms.ComboBox();
+            this.ShowLoadCombinationsBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(57, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(553, 102);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Hellow World!";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ShowReactionForcesBtn
+            // 
+            this.ShowReactionForcesBtn.Location = new System.Drawing.Point(57, 169);
+            this.ShowReactionForcesBtn.Name = "ShowReactionForcesBtn";
+            this.ShowReactionForcesBtn.Size = new System.Drawing.Size(154, 48);
+            this.ShowReactionForcesBtn.TabIndex = 1;
+            this.ShowReactionForcesBtn.Text = "Show Reaction Forces";
+            this.ShowReactionForcesBtn.UseVisualStyleBackColor = true;
+            this.ShowReactionForcesBtn.Click += new System.EventHandler(this.ShowReactionFrocesBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 247);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(985, 277);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // LoadCombinationComBox
+            // 
+            this.LoadCombinationComBox.FormattingEnabled = true;
+            this.LoadCombinationComBox.Location = new System.Drawing.Point(471, 196);
+            this.LoadCombinationComBox.Name = "LoadCombinationComBox";
+            this.LoadCombinationComBox.Size = new System.Drawing.Size(506, 21);
+            this.LoadCombinationComBox.TabIndex = 3;
+            // 
+            // ShowLoadCombinationsBtn
+            // 
+            this.ShowLoadCombinationsBtn.Location = new System.Drawing.Point(233, 169);
+            this.ShowLoadCombinationsBtn.Name = "ShowLoadCombinationsBtn";
+            this.ShowLoadCombinationsBtn.Size = new System.Drawing.Size(154, 48);
+            this.ShowLoadCombinationsBtn.TabIndex = 4;
+            this.ShowLoadCombinationsBtn.Text = "Show Load Combo";
+            this.ShowLoadCombinationsBtn.UseVisualStyleBackColor = true;
+            this.ShowLoadCombinationsBtn.Click += new System.EventHandler(this.ShowLoadCombinationsBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 583);
+            this.Controls.Add(this.ShowLoadCombinationsBtn);
+            this.Controls.Add(this.LoadCombinationComBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ShowReactionForcesBtn);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "My First Plugin";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ShowReactionForcesBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox LoadCombinationComBox;
+        private System.Windows.Forms.Button ShowLoadCombinationsBtn;
+    }
+}
---
+++ 03-Learning C#/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ETABSv1;
+
+namespace ETABS_Plugin
+{
+    public partial class Form1 : Form
+    {
+        private cPluginCallback _Plugin = null;
+        private cSapModel _SapModel = null;
+        
+        //initiate lists
+        List<LoadCombination> LoadCombinationList;
+        List<JointReaction> JointReactionList;
+
+
+        public Form1(ref cSapModel SapModel, ref cPluginCallback Plugin)
+        {
+            _Plugin = Plugin;
+            _SapModel = SapModel;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // do setup things here
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // must include a call to finish()
+            _Plugin.Finish(0);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowLoadCombinationsBtn_Click(object sender, EventArgs e)
+        {
+            int NumberNames = 1;
+            string[] Myname = null;
+
+            _SapModel.RespCombo.GetNameList(ref NumberNames, ref Myname);
+
+            LoadCombinationList = new List<LoadCombination>();
+
+            for (int i = 0; i < Myname.Length; i++)
+            {
+                LoadCombination LComb = new LoadCombination();
+                LComb.NumberNames = NumberNames;
+                LComb.MyName = Myname[i];
+                LoadCombinationComBox.Items.Add(Myname[i]);
+
+      
+
+                LoadCombinationList.Add(LComb);
+            }
+            dataGridView1.DataSource = LoadCombinationList;
+        }
+
+        private void ShowReactionFrocesBtn_Click(object sender, EventArgs e)
+        {
+            string Name = "";
+            eItemTypeElm ItemTypeElm;
+            int NumberResults = 1;
+            string[] Obj = null;
+            string[] Elm = null;
+            string[] LoadCase = null;
+            string[] StepType = null;
+            double[] StepNum = null;
+            double[] F1 = null;
+            double[] F2 = null;
+            double[] F3 = null;
+            double[] M1 = null;
+            double[] M2 = null;
+            double[] M3 = null;
+            int x = -1;
+            _SapModel.Results.Setup.DeselectAllCasesAndCombosForOutput();
+            //Method below acts on a string
+            _SapModel.Results.Setup.SetComboSelectedForOutput(LoadCombinationComBox.SelectedItem.ToString());
+
+            //unsure why eItemTypeElm.Element does not need to be initiated
+            //anything that is ref is returned, not input
+            x = _SapModel.Results.JointReact("4", eItemTypeElm.Element, ref NumberResults, ref Obj, ref Elm, ref LoadCase, ref StepType, ref StepNum, ref F1, ref F2, ref F3, ref M1, ref M2, ref M3);
+
+            JointReactionList = new List<JointReaction>();
+            //this class is from the other class that we created in a seperate file
+            JointReaction JReact = new JointReaction();
+            
+            JReact.Name = Name;
+            JReact.LoadCase = LoadCase[0];
+            JReact.F1 = F1[0];
+            JReact.F2 = F2[0];
+            JReact.F3 = F3[0];
+            JReact.M1 = M1[0];
+            JReact.M2 = M2[0];
+            JReact.M3 = M3[0];
+
+            JointReactionList.Add(JReact);
+            dataGridView1.DataSource = JointReactionList;
+
+
+        }
+    }
+}
